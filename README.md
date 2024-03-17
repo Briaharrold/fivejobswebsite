e5f255ecee0eb5f91625b58b6a13f796348673a6
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-03-17 11:32:07 -0700

    FashionNva

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Bria Harrold
 1-17-2024
-I made a website about the jobs I want!!!
+I made a website about the jobs I want!!! I used react bootstrap, and routes too create application that shows 5 places I would like to work at after CodeStack.
 peer reviewer: Jeriah Marson
 peer review : I like the icons and the image in the nav bar, gives all the information and has the required 5 jobs.
--- src/Components/pages/JobTwoComponent.tsx
+++ src/Components/pages/JobTwoComponent.tsx
@@ -23,6 +23,15 @@ const JobTwoComponent = () => {
       <p className="pt-3 padding-text text-center">
       Becoming a freelance software engineer will give me the flexibility and the unique ability to create my projects to support my educational goals, particularly in teaching kids to code with ASL integration. This will allow me schedule my work around my teaching commitments and focus on creating an inclusive coding curriculum. It also exposes me to various technologies and projects, enhancing my skill set, which I will then pass on to my students. Additionally, networking opportunities will be essential for growing my business by connecting with like-minded educators, organizations, and communities interested in accessible coding education.
       </p>
+      <h3 className="pt-3  text-center">
+      <Link
+              to="https://www.fashionnova.com/pages/careers"
+              target="_blank"
+            >
+              Click Here
+              
+            </Link>
+           </h3>
       <h3
       className="pt-3"
       style={{ display: "flex", justifyContent: "center", color:'rgb(28, 0, 103'}}
@@ -62,6 +71,10 @@ const JobTwoComponent = () => {
     </Col>
     </Row>
     <Row>
+      <Col>
+      
+      
+      </Col>
 </Row>
  
 
